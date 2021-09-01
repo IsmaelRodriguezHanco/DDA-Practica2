ecd08681a6bcb759f1b7714321781d5923b0a082
IsmaelRodriguezHanco <70766663@ucsm.edu.pe>
2021-09-01 00:56:15 -0500

    Implementacion de la experiencia 1,2 y 3 de la practica, incluyendo el ejercicio propuesto

---
+++ EjercicioPropuestoDeLISTAS/Site.Master.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EjercicioPropuestoDeLISTAS
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (this.IsPostBack == false)
+            {
+                Electrodomesticos.Items.Add(new ListItem("Refrigedora", "1"));
+                Electrodomesticos.Items.Add(new ListItem("Televisor", "2"));
+                Electrodomesticos.Items.Add(new ListItem("Microondas", "3"));
+                Electrodomesticos.Items.Add(new ListItem("Lavadora", "4"));
+
+            }
+            Grafico.Visible = false;
+        }
+        protected void Convertir_ServerClick(Object sender, EventArgs e)
+        {
+            
+            decimal conversion = 0;
+            ListItem elemento = Electrodomesticos.Items[Electrodomesticos.SelectedIndex];
+            if (elemento.Value == "1")
+            {
+                
+                Resultado.InnerText = " Codigo ---------- nombre ---------- descripcion----------  marca---------- precio";
+                Resultado2.InnerText = "15687 ----------LG 679L----------No Frost LM85SXD Negro-----LG -------------8199";
+            }
+            if (elemento.Value == "2")
+            {
+                Resultado.InnerText = " Codigo ---------- nombre ---------- descripcion----------  marca---------- precio";
+                Resultado2.InnerText = "19874 -------- SAMSUNG CRYSTAL -----UHD 70 Ultra HD ------SAMSUNG---------- 2799";
+            }
+            if (elemento.Value == "3")
+            {
+                Resultado.InnerText = " Codigo ---------- nombre ---------- descripcion----------  marca---------- precio ";
+                Resultado2.InnerText = "98523 ---------- WINIA 28L------- WMN-28MD Gris Claro------WINIA-----------269";
+            }
+            if (elemento.Value == "4")
+            {
+                Resultado.InnerText = " Codigo ---------- nombre ---------- descripcion----------  marca---------- precio ";
+                Resultado2.InnerText = "24653---------- Lavadora LG ------17Kg Negro Platino------- LG ----------- 2399";
+            }
+           
+
+        }
+        protected void MostrarGrafico_ServerClick(Object sender, EventArgs e)
+        {
+            Grafico.Src = "figura" + Electrodomesticos.SelectedIndex.ToString() + ".jpg";
+            Grafico.Alt = "Grafico Del Aparato";
+            Grafico.Visible = true;
+        }
+
+    }
+}
---
+++ EjercicioPropuestoDeLISTAS/Site.Master.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace EjercicioPropuestoDeLISTAS
+{
+
+
+    public partial class SiteMaster
+    {
+
+        /// <summary>
+        /// Control Form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm Form1;
+
+        /// <summary>
+        /// Control Electrodomesticos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlSelect Electrodomesticos;
+
+        /// <summary>
+        /// Control Submit1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputSubmit Submit1;
+
+        /// <summary>
+        /// Control Convertir.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Convertir;
+
+        /// <summary>
+        /// Control Resultado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Resultado;
+
+        /// <summary>
+        /// Control Output2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Output2;
+
+        /// <summary>
+        /// Control Resultado2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Resultado2;
+
+        /// <summary>
+        /// Control MostrarGrafico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputSubmit MostrarGrafico;
+
+        /// <summary>
+        /// Control Grafico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlImage Grafico;
+
+        /// <summary>
+        /// Control bodyContent.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder bodyContent;
+    }
+}
